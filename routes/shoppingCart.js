988d53b5bd6fe75400e7ac90e94fa1517a6fa5b3
Eric Er Chin Ann <er_chin_ann@hotmail.com>
2023-01-11 06:21:31 +0000

    defensive coding

--- routes/shoppingCart.js
+++ routes/shoppingCart.js
@@ -5,7 +5,12 @@ const router = express.Router();
 const CartServices = require('../services/cart_services');
 
 router.get('/', async(req,res)=>{
+
+if(req.session.customer.id){
+
     let cart = new CartServices(req.session.customer.id);
+    // let cart = new CartServices(2);
+    // console.log(req)
 
     let asd = (await cart.getCart())
 
@@ -14,10 +19,21 @@ router.get('/', async(req,res)=>{
     res.render('cart/index', {
         'shoppingCart': (await cart.getCart()).toJSON()
     })
+
+} else {
+
+ req.flash('error_messages', 'Access denied')
+ res.redirect('/products')
+
+}
+
 })
 
 
 router.get('/:product_id/add', async (req,res)=>{
+
+    if(req.session.customer.id){
+
     let cart = new CartServices(req.session.customer.id);
 
     await cart.addToCart(req.params.product_id, 1);
@@ -25,24 +41,54 @@ router.get('/:product_id/add', async (req,res)=>{
     req.flash('success_messages', 'Successfully added to cart')
 
     res.redirect('/products')
+
+    } else {
+
+        req.flash('error_messages', 'Access denied')
+        res.redirect('/products')
+
+    }
+
 })
 
 
 router.get('/:product_id/remove', async(req,res)=>{
+if(req.session.customer.id){
+
     let cart = new CartServices(req.session.customer.id);
     req.flash("success_messages", `Item has been removed`);
     
     await cart.remove(req.params.product_id);
     res.redirect('/cart');
+
+} else {
+
+    req.flash('error_messages', 'Access denied')
+    res.redirect('/products')
+
+}
+
+
 })
 
 
 
 router.post('/:product_id/quantity/update', async(req,res)=>{
+
+    if(req.session.customer.id){
+
     let cart = new CartServices(req.session.customer.id);
     await cart.setQuantity(req.params.product_id, req.body.newQuantity);
     req.flash("success_messages", "Quantity updated")
     res.redirect('/cart');
+        
+    } else {
+
+        req.flash('error_messages', 'Access denied')
+        res.redirect('/products')
+
+    }
+
   })
 
 
